ab86a9972affe0f58d2173755a5edf94c7f2f7cb
Leonardo <lfotecam@gmail.com>
2019-02-24 22:00:37 -0500

    upload images and save mongo

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,16 @@
 const { Router } =  require('express');
 const router = Router();
+const photo = require('../models/photos');
+const cloudinary = require('cloudinary');
+const fs = require('fs');
+const { promisify } = require('util');
+const deleteFile = promisify(fs.unlink);
 
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+})
 router.get('/', (req,res)=>{
     res.render('images');
 })
@@ -8,5 +18,17 @@ router.get('/', (req,res)=>{
 router.get('/images/add', function renderAddImages(req, res){
     res.render('image_form');
 })
-
+router.post('/images/add', async function addImages(req,res){
+    const { title, description } = req.body;
+    const result = await cloudinary.v2.uploader.upload(req.file.path);
+    const newPhoto = new photo({
+         title,
+         description,
+         image_url: result.url,
+         public_id: result.public_id
+     })
+    await newPhoto.save();
+    await deleteFile(req.file.path);
+    res.send('uploaded image')
+})
 module.exports = router;
